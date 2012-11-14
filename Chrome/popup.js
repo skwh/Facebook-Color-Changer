56641902c88f1e9d0cb3c95864406f2ad7809dcb
skwh <somekidwithhtml@gmail.com>
2012-11-13 23:08:24 -0700

    Added checking of the hex value

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -1,10 +1,18 @@
 function setColor(e) {
+	var isOk  = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/;
 	var color = e.target.getAttribute("data-path");
 	if (color == "inputBox") {
 		color = document.getElementById('hex').value;	
 	}
+	color = color.split("");
+	color.unshift("#");
+	color = color.join("");
+	console.log(color);
+	if (color == "#" || color == "Please Enter a value!" || !isOk.test(color)) {
+		document.getElementById('hex').value = "Please enter a correct hexadecimal value!";	
+	}
 	executeScript(null,
-		{code:"document.getElementById('blueBar').style.backgroundColor = \'#" + color + "\'"});
+		{code:"document.getElementById('blueBar').style.backgroundColor = \'" + color + "\'"});
 	console.log(color);
 	console.log(e.target.getAttribute("data-path"));
 }
