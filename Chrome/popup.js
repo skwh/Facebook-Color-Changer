9d72024a533b88c6982ef9cdac6f73687b998b44
skwh <somekidwithhtml@gmail.com>
2012-11-13 22:52:47 -0700

    Added/Fixed EVERYTHING

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -1,7 +1,19 @@
-function setColor(color) {
-	var c;
-	if (color == 'default') {
-		c = "#3B5999";
-		setElementAsBGcolor(document.getElementById('blueBar'),c);
+function setColor(e) {
+	var color = e.target.getAttribute("data-path");
+	if (color == "inputBox") {
+		color = document.getElementById('hex').value;	
 	}
+	executeScript(null,
+		{code:"document.getElementById('blueBar').style.backgroundColor = \'#" + color + "\'"});
+	console.log(color);
+	console.log(e.target.getAttribute("data-path"));
 }
+function executeScript(tabId, InjectDetails, callback) {
+	chrome.tabs.executeScript(tabId,InjectDetails,callback);	
+}
+document.addEventListener("DOMContentLoaded",function() {
+	var buttons = document.querySelectorAll("button");
+	for (var i=0;i<buttons.length;i++) {
+		buttons[i].addEventListener('click',setColor);	
+	}
+});
