5c6c7de6ac909b0e64ae5c5d84d06a5dff43ff93
skwh <somekidwithhtml@gmail.com>
2012-11-15 07:17:36 -0700

    Working on automatic calling of function by 'input' event

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -1,13 +1,21 @@
 function setColor(e) {
-	var isOk  = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/;
-	var color = e.target.getAttribute("data-path");
+	var isOk  = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/, color;
+	console.log(e.type);
+	if (e.type == "HTMLEvent") {
+		color = document.getElementById('hex').value;	
+	} else {
+		color = e.target.getAttribute("data-path");
+		if (color == "inputBox") {
+			color = document.getElementById('hex').value;	
+		}	
+	}
+	/*var color = e.target.getAttribute("data-path");
 	if (color == "inputBox") {
 		color = document.getElementById('hex').value;	
-	}
+	}*/
 	color = color.split("");
 	color.unshift("#");
 	color = color.join("");
-	console.log(color);
 	if (color == "#" || color == "Please Enter a value!" || !isOk.test(color)) {
 		document.getElementById('hex').value = "Please enter a correct hexadecimal value!";	
 	}
@@ -25,11 +33,13 @@ document.addEventListener("DOMContentLoaded",function() {
 	for (var i=0;i<buttons.length;i++) {
 		buttons[i].addEventListener('click',setColor);	
 	}
+	document.getElementById('hex').addEventListener('input',setColor);
 	if (typeof(Storage) !== "undefined") {
 		var pref = localStorage.getItem("FBCOLORS");
 		if (!(pref == null || pref == "")) {
 			executeScript(null,
 						{code:"document.getElementById('blueBar').style.backgroundColor = \'" + pref + "\'"});
+			document.getElementById('hex').value = pref;
 		}
 	}
 });
